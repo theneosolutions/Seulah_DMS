f4ad7f4eb465ea1610a7a0ae9657804d7ef80056
nabigh9091 <152377309+nabigh9091@users.noreply.github.com>
2023-12-08 09:51:46 +0500

    update path

--- src/main/java/com/seulah/seulahdms/controller/AdminApiResponseController.java
+++ src/main/java/com/seulah/seulahdms/controller/AdminApiResponseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("apiResponse")
+@RequestMapping("api/v1/dms/apiResponse")
 @Slf4j
 public class AdminApiResponseController {
 
