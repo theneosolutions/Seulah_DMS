e3cf06cab3a3ef794a135b46a99973e016be3f57
nabigh9091 <152377309+nabigh9091@users.noreply.github.com>
2023-12-13 15:15:55 +0500

    added api info for swagger

--- src/main/java/com/seulah/seulahdms/config/OpenApiConfig.java
+++ src/main/java/com/seulah/seulahdms/config/OpenApiConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Collections;
 import java.util.function.Predicate;
 
 @Configuration
@@ -20,6 +23,15 @@ public class OpenApiConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .paths(Predicate.not(PathSelectors.regex("/error.*")))
-                .build();
+                .build().apiInfo(apiInfo());
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Seualh REST API",
+                "DMS Api Documentation.",
+                "V1",
+                "",
+                new Contact("", "", ""),
+                "", "", Collections.emptyList());
     }
 }
