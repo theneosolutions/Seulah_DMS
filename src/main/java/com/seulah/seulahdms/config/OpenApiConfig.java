4efeaeb1a69d28fdb2a4b2586d721210c1b16ca1
nabigh9091 <152377309+nabigh9091@users.noreply.github.com>
2023-12-13 13:18:45 +0500

    update code for swagger

---
+++ src/main/java/com/seulah/seulahdms/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.seulah.seulahdms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.function.Predicate;
+
+@Configuration
+@EnableWebMvc
+public class OpenApiConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .paths(Predicate.not(PathSelectors.regex("/error.*")))
+                .build();
+    }
+}
